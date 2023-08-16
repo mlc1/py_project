35902a448833b14082ea9e61fd7259c10b97f460
manu <manu.lc46@gmail.com>
2023-08-16 03:06:27 +0200

    docker

--- web_server/main.py
+++ web_server/main.py
@@ -1,4 +1,28 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()#Creamos la instacia
+
+'''
+Creamos el decorador y le decimos cual es la 
+ruta por la cual desde la web van a poder ingresar
+'''
+@app.get('/',response_class=HTMLResponse)
+def get_list():
+    return"""
+         <h1 style='text-align:center; color:blue'>Soy un titulo</h1>
+
+    """
+
+@app.get('/contact')
+def get_list():
+    
+    return {'name': 'Juan'}
+
+
+
+
 
 def run():
     store.get_categories()
